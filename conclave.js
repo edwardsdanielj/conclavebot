d22f79debddf91fa3c8c0634e704af942d0a965d
Joseph LaCava <jpl4k@uvawise.edu>
2017-08-08 20:16:10 -0400

    init conclave bot
    init conclave bot

---
+++ conclave.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const ConclaveBot = require('discord-bot-base');
+const ImgurSesh = require('imgur-search');
+const GiphySesh = require('giphy-api');
+
+var discordVars = require('./config/discord.js');
+var imgurVars = require('./config/imgur.js');
+var giphyVars = require('./config/imgur.js');
