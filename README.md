29589be7b6bb1176d698aab3961eb9e7038f40f4
Joseph LaCava <jpl4k@uvawise.edu>
2017-08-08 22:12:21 -0400

    Add Travis CI integration

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.org/jpl154/conclavebot.svg?branch=master)](https://travis-ci.org/jpl154/conclavebot)
+
+# conclave-bot
+A simple Discord bot
--- conclave.js
+++ conclave.js
@@ -4,6 +4,6 @@ const ConclaveBot = require('discord-bot-base');
 const ImgurSesh = require('imgur-search');
 const GiphySesh = require('giphy-api');
 
-var discordVars = require('./config/discord.js');
-var imgurVars = require('./config/imgur.js');
-var giphyVars = require('./config/imgur.js');
+var discordVars = require('./config/discord.json');
+var imgurVars = require('./config/imgur.json');
+var giphyVars = require('./config/giphy.json');
