51cabf7e3161f581fa64d11df26b59be06b604f6
Joseph LaCava <jpl4k@uvawise.edu>
2017-08-11 16:10:00 -0400

    Change to discord.js

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-[![Build Status](https://travis-ci.org/jpl154/conclavebot.svg?branch=master)](https://travis-ci.org/jpl154/conclavebot)
+[![Build Status](https://travis-ci.org/jpl154/conclavebot.svg?branch=development)](https://travis-ci.org/jpl154/conclavebot)
 
 # conclave-bot
 A simple Discord bot
 
 # IN DEVELOPMENT
+## Useful Documentation
+[DiscordAPI](https://discordapp.com/developers/docs/intro)
--- conclave.js
+++ conclave.js
@@ -1,14 +1,13 @@
 'use strict';
 
 //init modules
-const ConclaveBot = require('discord-bot-base');
+const ConclaveBot = require('discord.js');
 const ImgurSesh = require('imgur-search');
 const GiphySesh = require('giphy-api');
 
 //init env vars
-var discordVars = require('./config/discord.js');
-var imgurVars = require('./config/imgur.js');
-var giphyVars = require('./config/giphy.js');
+var discordVars = require('./bot/config/disconfig.js');
+var imgurVars = require('./bot/config/imgur.js');
+var giphyVars = require('./bot/config/giphy.js');
 
 //init Discord bot
-//var ccbot = new ConclaveBot.Bot('dev', true,)
